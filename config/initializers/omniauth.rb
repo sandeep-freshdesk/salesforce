40bdb4d73e10187d162b4957ee324274e1c3b358
sandeep-freshdesk <sandeep@freshdesk.com>
2017-02-16 14:23:05 +0530

    adding omniauth.rb file

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder  do
+	provider :salesforce, "3MVG9YDQS5WtC11rEEbuE0ugmmxoFqE2nwkWgPIMnmmss56M7Lc90J0wULBI3_zBVbQy7Fh9ralCsTnz7DGt6", "201586777941315632"
+end
